09201f783a207a51730aecfb13dfeaba6624eeea
shodliktest <shodlikfire@gmail.com>
2025-12-10 10:04:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const AUTH_KEY = 'test_ijtimoiyyashodlikauthenticated';
 const VALID_CREDENTIALS = {
     username: 'Ijtimoiy2026',
-    password: 'test1221'
+    password: 'Odina1221'
 };
 
 // ===== TEST SAVOLLARI (400 TA) =====
@@ -4298,5 +4298,6 @@ function updateTimer() {
     document.getElementById('timer').textContent = formattedTime;
 
 }
+
 
 
