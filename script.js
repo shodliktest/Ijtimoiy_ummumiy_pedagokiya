ee4c6cc80d46801cafffb44445087b5b04124064
shodliktest <shodlikfire@gmail.com>
2025-12-10 21:05:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ===== PAROL TIZIMI =====
 const AUTH_KEY = 'test_ijtimoiyyashodlikauthenticated';
 const VALID_CREDENTIALS = {
-    username: '1111',
-    password: '1111'
+    username: '11112222',
+    password: '11112222'
 };
 
 // ===== TEST SAVOLLARI (400 TA) =====
@@ -4298,6 +4298,7 @@ function updateTimer() {
     document.getElementById('timer').textContent = formattedTime;
 
 }
+
 
 
 
